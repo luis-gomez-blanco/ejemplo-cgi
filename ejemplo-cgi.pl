bd49ff65cdaaf18de818a15a1b0b0264a97fbcd5
luis-gomez-blanco <luis.gomezblanco@educa.madrid.org>
2024-09-24 13:30:18 +0200

    Update ejemplo-cgi.pl

--- ejemplo-cgi.pl
+++ ejemplo-cgi.pl
@@ -1,25 +1,13 @@
-#!"C:\xampp\perl\bin\perl.exe"
-
-#Cabecera
+#!"E:\xampp\perl\bin\perl.exe"
+$HORA = localtime();
 print "Content-Type: text/html\n\n";
-
-use strict;
-use warnings;
-
-#Cuerpo HTML
-print "<!DOCTYPE HTML>\n";
+print "<!DOCTYPE html>\n";
 print "<html>\n";
 print "<head>\n";
-print "<title>Ejemplo de un CGI programado en PERL</title>\n";
+print "<title>Prueba CGI</title>\n";
 print "</head>\n";
 print "<body>\n";
-print "<h1>Hola Mundo</h1>\n";
-print "Server Name: $ENV{'SERVER_NAME'} <br/>";
-print "Server Port: $ENV{'SERVER_PORT'} <br/>";
-print "Server Software: $ENV{'SERVER_SOFTWARE'} <br/>";
-print "Server Protocol: $ENV{'SERVER_PROTOCOL'} <br/>";
-print "CGI VERSION: $ENV{'GATEWAY_INTERFACE'} <br/>";
-print "Direccion IP del cliente: $ENV{'REMOTE_ADDR'}";
-
-print "</body>";
+print "<p>Hola $ENV{REMOTE_ADDR}</p>\n";
+print "<p>$HORA</p>\n";
+print "</body>\n";
 print "</html>";
